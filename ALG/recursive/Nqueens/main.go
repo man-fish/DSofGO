fc6af52ce9996f040f6c66c28974998f7217c160
inno,com <893930581@qq.com>
2019-12-23 00:11:53 +0800

    N皇后问题

--- ALG/recursive/Nqueens/main.go
+++ ALG/recursive/Nqueens/main.go
@@ -1,35 +1,70 @@
 package main
 
-import "fmt"
-
-type checkboard [4][4]int
+import (
+	"fmt"
+	"math"
+)
 
 /**
-	0 没有试过
-	1 表示成功放置皇后
-	2 表示位置冲突
+	N皇后问题
  */
 
-func setQueen(board checkboard,i int,j int) bool {
-	for f := 0;f < len(board[3]) ;f++  {
-		if board[3][f] == 1 {
-			return  true
-		}
+func solveNQueens(n int) [][]string {
+	ans := [][]string{}
+	checkboard := make([]int, n)
+	calcQueues(&ans,n,checkboard,0)
+	return ans
+}
+	/**
+		ans 	*[][]string		全部题解
+		n 		int				兼容皇后数量
+		queues 	[]int			本轮皇后排列情况
+		row 	int				轮数
+	 */
+func calcQueues(ans *[][]string,n int,queues []int,row int) {
+	if row == n {
+		q := printQ(queues, n)
+		*ans = append(*ans, q)
+		return
 	}
 
-	if board[i][j] == 0 {
-
-	}else{
-		return false
+	for col := 0; col < n; col++ {
+		queues[row] = col
+		if isValid(queues,row) {
+			calcQueues(ans,n,queues,row+1)
+		}
 	}
 }
 
-func main() {
-	var board checkboard
-	for i:=0;i<4 ;i++ {
-		for j:=0;j<4 ;j++  {
-			fmt.Print(board[i][j],"\t")
+func isValid(queues []int,row int) bool {
+	for i := 0;i < row; i++ {
+		if queues[i] == queues[row] || math.Abs(float64(row-i)) == math.Abs(float64(queues[row] - queues[i])){
+			return false
 		}
-		fmt.Println()
+	}
+	return true
+}
+
+func printQ(res []int, n int) []string {
+	s := []string{}
+	for _, v := range res {
+		str := ""
+		for i := 0; i < n; i++ {
+			if i == v {
+				str += "Q"
+			} else {
+				str += "."
+			}
+		}
+		s = append(s, str)
+	}
+	return s
+}
+
+func main() {
+	for _,col := range solveNQueens(4) {
+		for _,row := range col{
+			fmt.Println(row)
+		}
 	}
 }
