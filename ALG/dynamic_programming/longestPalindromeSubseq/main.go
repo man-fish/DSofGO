8442f0607107acd206c667ded64c1dc53121dc92
inno,com <893930581@qq.com>
2020-01-23 18:41:49 +0800

    项目迁移

---
+++ ALG/dynamic_programming/longestPalindromeSubseq/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+func longestPalindromeSubseq(s string) int {
+
+}
+
+func lpssRecursive(s string) int {
+	finalResult := math.MinInt64
+
+	for start := 0; start < len(s); i++ {
+		for end := s; ;  {
+
+		}
+		if isPalindromeSubseq(s, start, i) {
+			finalResult = int(math.Max(float64(finalResult),float64(lpssRecursive(s, start+1))))
+		}
+	}
+}
+
+func isPalindromeSubseq(s string, start,end int) bool {
+	return true
+}
+
+/*
+
+*/
